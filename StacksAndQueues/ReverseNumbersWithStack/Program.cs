57f54029837b83a911e5b69b0a35d8a301683a3d
dartchemist <slav.petev@gmail.com>
2016-05-17 23:22:10 +0300

    Solved Problem ReverseNumberWithStack

--- StacksAndQueues/ReverseNumbersWithStack/Program.cs
+++ StacksAndQueues/ReverseNumbersWithStack/Program.cs
@@ -10,6 +10,23 @@ namespace ReverseNumbersWithStack
     {
         static void Main(string[] args)
         {
+            var input = Console.ReadLine().Split(
+                new [] {' '}, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse)
+                .ToArray();
+
+            var reversedInput = Reverse(input);
+            Console.WriteLine(reversedInput);
+        }
+
+        private static string Reverse(int[] input)
+        {
+            var inputStack = new Stack<int>();
+            foreach (var element in input)
+            {
+                inputStack.Push(element);
+            }
+            
+            return string.Join(" ", inputStack.ToArray());
         }
     }
 }
