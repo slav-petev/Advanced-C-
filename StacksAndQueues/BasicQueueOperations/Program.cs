acabc91b1e00f60474c346e446042298fd8c113a
Slav Petkov <slav.petev@gmail.com>
2016-05-18 11:30:34 +0300

    Implemented basicQueueOperations

---
+++ StacksAndQueues/BasicQueueOperations/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicQueueOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+            var numberOfElementsToEnque = input[0];
+            var numberOfElementsToDeque = input[1];
+            var elementToCheck = input[2];
+
+            var queue = new Queue<int>();
+            var elementsToEnqueue = ReadElementsToEnqueue(numberOfElementsToEnque);
+            EnqueueElements(queue, elementsToEnqueue);
+            DequeueElements(queue, numberOfElementsToDeque);
+            CheckElement(queue, elementToCheck);
+        }
+
+        private static int[] ReadElementsToEnqueue(int numberOfElementsToEnqueue)
+        {
+            var elementsToEnqueue = Console.ReadLine();
+            
+            return elementsToEnqueue.Trim().Split(
+                new[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+        }
+
+        private static void EnqueueElements(Queue<int> queue, int[] elementsToEnqueue)
+        {
+            foreach (var elementToEbqueue in elementsToEnqueue)
+            {
+                queue.Enqueue(elementToEbqueue);
+            }
+        }
+
+        private static void DequeueElements(Queue<int> queue, int numberOfElementsToDequeue)
+        {
+            for (var i = 0; i < numberOfElementsToDequeue; ++i)
+            {
+                queue.Dequeue();
+            }
+        }
+
+        private static void CheckElement(Queue<int> queue, int elementToCheck)
+        {
+            if (queue.Count == 0)
+            {
+                Console.WriteLine("0");    
+            }
+            else if (queue.Contains(elementToCheck))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(queue.Min());
+            }
+        }
+    }
+}
