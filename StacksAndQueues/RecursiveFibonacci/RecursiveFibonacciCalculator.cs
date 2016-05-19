b244be6a1071fff09d5aad222e839397b7359c1e
Slav Petkov <slav.petev@gmail.com>
2016-05-19 16:12:39 +0300

    Solved problem Recursive Fibonacci

---
+++ StacksAndQueues/RecursiveFibonacci/RecursiveFibonacciCalculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class RecursiveFibonacciCalculator
+{
+    private static readonly Dictionary<int, long> _lookupTable =
+        new Dictionary<int, long>();
+
+    public static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(CalculateFibonnaciNumber(n));
+    }
+
+    private static long CalculateFibonnaciNumber(int n)
+    {
+        if (n == 0)
+        {
+            return 0L;
+        }
+        if (n == 1)
+        {
+            return 1L;
+        }
+
+        if (_lookupTable.ContainsKey(n))
+        {
+            return _lookupTable[n];
+        }
+        var currentFibonacciNumber = CalculateFibonnaciNumber(n - 1) + CalculateFibonnaciNumber(n - 2);
+        _lookupTable[n] = currentFibonacciNumber;
+        return currentFibonacciNumber;
+    }
+}
