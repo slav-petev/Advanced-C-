249a5e6f6a6dd007c8ec92e77d76b349a9825258
Slav Petkov <slav.petev@gmail.com>
2016-05-19 15:51:16 +0300

    Solved Problem Balanced parentheses

---
+++ StacksAndQueues/BalancedParenthesis/ParenthesesbalancedChecker.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Policy;
+
+public class ParenthesesbalancedChecker
+{
+    public static void Main()
+    {
+        var expression = Console.ReadLine();
+        var openingParenthesesStack = new Stack<char>();
+        var isExpressionBalanced = true;
+
+        var index = 0;
+        while (index < expression.Length && isExpressionBalanced)
+        {
+            var currentSymbol = expression[index];
+            if (IsOpeningParentheses(currentSymbol))
+            {
+                openingParenthesesStack.Push(currentSymbol);
+            }
+            else
+            {
+                if (openingParenthesesStack.Count == 0)
+                {
+                    isExpressionBalanced = false;
+                }
+                else
+                {
+                    var currentOpeningparentheses = openingParenthesesStack.Pop();
+                    if (!AreParenthesesMatched(currentOpeningparentheses, currentSymbol))
+                    {
+                        isExpressionBalanced = false;
+                    }
+                }
+            }
+            ++index;
+        }
+
+        Console.WriteLine(isExpressionBalanced ? "YES" : "NO");
+    }
+
+    private static bool IsOpeningParentheses(char parentheses)
+    {
+        return parentheses == '(' || parentheses == '[' || parentheses == '{';
+    }
+
+    private static bool AreParenthesesMatched(char openingParentheses, char closingParentheses)
+    {
+        return (openingParentheses == '(' && closingParentheses == ')') ||
+            (openingParentheses == '[' && closingParentheses == ']') ||
+            (openingParentheses == '{' && closingParentheses == '}');
+    }
+}
