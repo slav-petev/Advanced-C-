fb127a9f5b9f8e03228e4a6a2dd941c0f4186e97
Slav Petkov <slav.petev@gmail.com>
2016-05-19 16:25:30 +0300

    Implemented StackFibonacci Calculator

---
+++ StacksAndQueues/StackFibonacci/StackFibonacciCalculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class StackFibonacciCalculator
+{
+    public static void Main()
+    {
+        var n = int.Parse(Console.ReadLine());
+        Console.WriteLine(CalculateFibonnaciNumber(n));
+    }
+
+    private static long CalculateFibonnaciNumber(int n)
+    {
+        if (n == 1 || n == 2)
+        {
+            return 1L;
+        }
+        var fibonnaciStack = new Stack<long>();
+        fibonnaciStack.Push(1);
+        fibonnaciStack.Push(1);
+
+        for (var i = 3; i <= n; ++i)
+        {
+            var firstPreviousFibonacciNumber = fibonnaciStack.Pop();
+            var secondPreviousFibonacciNumber = fibonnaciStack.Peek();
+            fibonnaciStack.Push(firstPreviousFibonacciNumber);
+            fibonnaciStack.Push(firstPreviousFibonacciNumber + secondPreviousFibonacciNumber);
+        }
+
+        return fibonnaciStack.Peek();
+    }
+}
