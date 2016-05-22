c8e64a5b4ea185098eef7ad0b78670f905052b11
dartchemist <slav.petev@gmail.com>
2016-05-22 13:17:11 +0300

    Sorted Problem PeriodicTable

---
+++ SetsAndDictionaries/03.PeriodicTable/PeriodicTableExcercise.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.PeriodicTable
+{
+    public class PeriodicTableExcercise
+    {
+        public static void Main(string[] args)
+        {
+            var chemicalCompoundsCount = int.Parse(Console.ReadLine());
+
+            var uniqueCompounds = new SortedSet<string>();
+
+            for (var i = 0; i < chemicalCompoundsCount; ++i)
+            {
+                var compounds = Console.ReadLine().Split(' ');
+                foreach (var compound in compounds)
+                {
+                    uniqueCompounds.Add(compound);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", uniqueCompounds));
+        }
+    }
+}
