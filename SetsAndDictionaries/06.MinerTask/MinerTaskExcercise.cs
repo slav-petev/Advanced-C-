40f057dbdeadd2355d4a7bbad9bc9d83ff72ccbe
dartchemist <slav.petev@gmail.com>
2016-05-22 14:32:29 +0300

    Solved Problem MinerTask

---
+++ SetsAndDictionaries/06.MinerTask/MinerTaskExcercise.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.MinerTask
+{
+    public class MinerTaskExcercise
+    {
+        public static void Main(string[] args)
+        {
+            var resourcesInfo = new Dictionary<string, long>();
+
+            var resourceName = Console.ReadLine();
+
+            while (resourceName != "stop")
+            {
+                var resourceQuantity = long.Parse(Console.ReadLine());
+
+                if (resourcesInfo.ContainsKey(resourceName))
+                {
+                    resourcesInfo[resourceName] += resourceQuantity;
+                }
+                else
+                {
+                    resourcesInfo.Add(resourceName, resourceQuantity);
+                }
+
+                resourceName = Console.ReadLine();
+            }
+
+            foreach (var resource in resourcesInfo)
+            {
+                Console.WriteLine($"{resource.Key} -> {resource.Value}");
+            }
+        }
+    }
+}
