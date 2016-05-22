58c2f6053c5e6ce1df3ad448f7e3d845087e544b
dartchemist <slav.petev@gmail.com>
2016-05-22 14:49:56 +0300

    Solved Problem FixEmails

---
+++ SetsAndDictionaries/FixEmails/FixEmailsExcercise.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixEmails
+{
+    public class FixEmailsExcercise
+    {
+        public static void Main()
+        {
+            var prohibitedDomains = new[] { "us", "uk" };
+            var usersEmails = new Dictionary<string, string>();
+
+            var username = Console.ReadLine();
+
+            while (username != "stop")
+            {
+                var userEmail = Console.ReadLine();
+                var domain = userEmail.Substring(
+                    userEmail.IndexOf('.') + 1);
+
+                if (!prohibitedDomains.Contains(domain.ToLower()))
+                {
+                    usersEmails.Add(username, userEmail);
+                }
+
+                username = Console.ReadLine();
+            }
+
+            foreach (var usersEmail in usersEmails)
+            {
+                Console.WriteLine($"{usersEmail.Key} -> {usersEmail.Value}");
+            }
+        }
+    }
+}
