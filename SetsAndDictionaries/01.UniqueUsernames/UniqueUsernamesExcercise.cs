bfe9fa2957bb4ab7311012118deec4c16012a3bc
dartchemist <slav.petev@gmail.com>
2016-05-22 12:59:49 +0300

    Solved Problem UniqueUsernames

---
+++ SetsAndDictionaries/01.UniqueUsernames/UniqueUsernamesExcercise.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.UniqueUsernames
+{
+    public class UniqueUsernamesExcercise
+    {
+        public static void Main(string[] args)
+        {
+            var usernamesCount = int.Parse(Console.ReadLine());
+            var usernames = new HashSet<string>();
+
+            for (var i = 0; i < usernamesCount; ++i)
+            {
+                usernames.Add(Console.ReadLine());
+            }
+
+            foreach (var username in usernames)
+            {
+                Console.WriteLine(username);
+            }
+        }
+    }
+}
