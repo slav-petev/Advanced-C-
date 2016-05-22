60464634673c26d8020384fdfe2fa744202e4cf9
dartchemist <slav.petev@gmail.com>
2016-05-22 13:09:57 +0300

    Solved problem SetsOfElements

---
+++ SetsAndDictionaries/02.SetsOfElements/SetsOfElementsExcercise.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.SetsOfElements
+{
+    public class SetsOfElementsExcercise
+    {
+        public static void Main(string[] args)
+        {
+            var setsLengths = Console.ReadLine().Split(' ')
+                .Select(int.Parse).ToArray();
+
+            var firstSetLength = setsLengths[0];
+            var secondSetLength = setsLengths[1];
+
+            var firstSet = CreateSet(firstSetLength);
+            var secondSet = CreateSet(secondSetLength);
+
+            firstSet.IntersectWith(secondSet);
+
+            Console.WriteLine(string.Join(" ", firstSet));
+        }
+
+        private static HashSet<int> CreateSet(int setLength)
+        {
+            var setOfElements = new HashSet<int>();
+            for (var i = 0; i < setLength; ++i)
+            {
+                setOfElements.Add(int.Parse(Console.ReadLine()));
+            }
+
+            return setOfElements;
+        }
+    }
+}
