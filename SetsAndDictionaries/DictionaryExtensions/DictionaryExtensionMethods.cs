813d2b0c493ba2e620e447764d3624bdf1b532eb
Slav Petkov <slav.petev@gmail.com>
2016-05-27 13:30:05 +0300

    Updated most projects to use AddOrUpdate extension method

---
+++ SetsAndDictionaries/DictionaryExtensions/DictionaryExtensionMethods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryExtensions
+{
+    public static class DictionaryExtensionMethods
+    {
+        public static void AddOrUpdate<TKey, TValue>(this IDictionary<TKey, TValue> dictionary,
+            TKey key, TValue value, Action<TValue> updateAction)
+        {
+            if (dictionary.ContainsKey(key))
+            {
+                updateAction(dictionary[key]);
+            }
+            else
+            {
+                dictionary.Add(key, value);
+            }
+        }
+
+        public static IDictionary<TKey, TValue> SortByValueDescending<TKey, TValue>(
+            this IDictionary<TKey, TValue> dictionary)
+        {
+            return dictionary.OrderByDescending(entry => entry.Value)
+                .ToDictionary(entry => entry.Key, entry => entry.Value);
+        }
+    }
+}
