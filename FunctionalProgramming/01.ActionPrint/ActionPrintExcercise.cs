0de25fd6494add9be81395b13117f5c4dd10dc78
Slav Petkov <slav.petev@gmail.com>
2016-06-08 15:08:27 +0300

    Solved problem ActionPrint

---
+++ FunctionalProgramming/01.ActionPrint/ActionPrintExcercise.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.ActionPrint
+{
+    public class ActionPrintExcercise
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine()
+                .Split()
+                .ToList();
+            input.ForEach(Console.WriteLine);
+        }
+    }
+}
