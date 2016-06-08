3c8a3e4d70b0149ec1260c7587f72cbf5ea0eda1
Slav Petkov <slav.petev@gmail.com>
2016-06-08 15:14:16 +0300

    Solved KnightsOfHonor problem

---
+++ FunctionalProgramming/02.KnightsOfHpnor/KnightsOfHonorExcercise.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.KnightsOfHpnor
+{
+    public class KnightsOfHonorExcercise
+    {
+        public static void Main()
+        {
+            var names = Console.ReadLine().Split(new[] { ' ' } , 
+                StringSplitOptions.RemoveEmptyEntries)
+            .ToList();
+
+            names.ForEach(name => Console.WriteLine($"Sir {name}"));
+        }
+    }
+}
